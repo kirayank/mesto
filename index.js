eb5fc3c4610b221dd786cea0101af275c8df62c6
Kira Iankieva <deerkira@gmail.com>
2022-03-15 19:54:03 +0500

    bug fixes

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 const editbutton = document.querySelector('.profile__editbutton');
-const form = document.querySelector('.popup__container');
+/*const form = document.querySelector('.popup__container');*/
+const popup = document.querySelector('.popup');
 const exitbutton = document.querySelector('.popup__exit');
-const savebutton = document.querySelector('.popup__save');
+const form = document.querySelector('.popup__editform');
 let nameInput = document.querySelector('#popup__input_name'); 
 let jobInput = document.querySelector('#popup__input_about');
 let nameProfile = document.getElementById('popup__profile_name'); //получила значение всей строки (что написано в заголовке)
 let aboutProfile = document.getElementById('popup__profile_about');
 
 
-function formOpen () {
-  form.classList.add('popup__container_opened');
-  nameInput.setAttribute('value', nameProfile.textContent); //присвоила значению инпута значение тега
-  jobInput.setAttribute('value', aboutProfile.textContent);
+function openForm () {
+  /*form.classList.add('popup__container_opened');*/
+  /*nameInput.setAttribute('value', nameProfile.textContent); //присвоила значению инпута значение тега
+  jobInput.setAttribute('value', aboutProfile.textContent);*/
+  nameInput.value = nameProfile.textContent; //присвоила значению инпута значение тега
+  jobInput.value = aboutProfile.textContent;
+  popup.classList.add('popup_opened');
 }
 
-function formClose () {
-  form.classList.remove('popup__container_opened');
+function closeForm () {
+  /*form.classList.remove('popup__container_opened');*/
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку форм
-  let name = nameInput.getAttribute('value');
-  let job = jobInput.getAttribute('value'); 
-  nameProfile.textContent = name;
-  aboutProfile.textContent = job;
-  formClose();
+  nameProfile.textContent = nameInput.value;
+  aboutProfile.textContent = jobInput.value;
+  closeForm();
 }
 
 
-editbutton.addEventListener('click', formOpen);
-exitbutton.addEventListener('click', formClose);
-savebutton.addEventListener('submit', formSubmitHandler);
+editbutton.addEventListener('click', openForm);
+exitbutton.addEventListener('click', closeForm);
+form.addEventListener('submit', formSubmitHandler);
 
 
 
